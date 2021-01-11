981032b974e481550c9aebf3dee1891df613f1eb
Olutayo Demilade Solana <65459000+allonsy-ai@users.noreply.github.com>
2021-01-11 22:47:36 +0100

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -64,20 +64,13 @@ def main():
 	
 
 	# Summarization
-st.subheader("Summarize Your Text")
+	if st.checkbox("Show Text Summarization"):
+		st.subheader("Summarize Your Text")
 
-message = st.text_area("Enter Text")
-if st.button("Summarize"):
-
-	if summary_options == 'sumy':
+		message = st.text_area("Enter Text")
+		if st.button("Summarize"):
 				summary_result = sumy_summarizer(message)
-	elif summary_options == 'gensim':
-				st.text("Using Gensim Summarizer ..")
-				summary_result = summarize(rawtext)
-	else:
-				st.warning("Using Default Summarizer")
-				st.text("Using Gensim Summarizer ..")
-				summary_result = summarize(rawtext)
+
 
 		
 	st.success(summary_result)
